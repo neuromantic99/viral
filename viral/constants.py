2a1985faa5e45318ca6476da20dd29da8602da00
neuromantic99 <jrowlandm@gmail.com>
2024-10-23 11:51:37 +0100

    update wheel circumference

--- viral/constants.py
+++ viral/constants.py
@@ -8,7 +8,8 @@ SERVER_PATH = Path("/Volumes/MarcBusche/James/")
 
 BEHAVIOUR_DATA_PATH = SERVER_PATH / Path("Behaviour/online/Subjects")
 
-WHEEL_CIRCUMFERENCE = 17 * math.pi  # cm
+WHEEL_CIRCUMFERENCE = 11 * math.pi  # cm
+print(f"WHEEL_CIRCUMFERENCE: {WHEEL_CIRCUMFERENCE}")
 ENCODER_TICKS_PER_TURN = 360  # check me
 
 SPREADSHEET_ID = "1fMnVXrDeaWTkX-TT21F4mFuAlaXIe6uVteEjv8mH0Q4"
